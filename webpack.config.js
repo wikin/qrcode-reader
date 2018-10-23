0e1abd86651b9137670be95409867bef9db6216d
miroslav-peterka <peterka@worklio.com>
2018-10-23 19:52:05 +0200

    docs

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'assets')
+    path: path.resolve(__dirname, 'docs')
   },
   module: {
     rules: [
