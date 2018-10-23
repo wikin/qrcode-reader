ea4fb06df507e7b202418d81495cab9570f32bcf
miroslav-peterka <peterka@worklio.com>
2018-10-23 19:48:14 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode:'production',
+  entry: {
+    main: './src/js/main.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'assets')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  } 
+};
